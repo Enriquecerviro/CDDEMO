04fa30ffb433915353f1ec2906c4ca3eb9b6dac5
Enrique Cervino <enrique.cervirodam@gmail.com>
2019-09-18 12:29:26 +0200

    dataService UpdatedInterfaces

--- src/app/pages/data.service.ts
+++ src/app/pages/data.service.ts
@@ -1,12 +1,47 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
+
+/**
+ * CREAMOS INTERFACES DE LOS COMPONENTES QUE TIENEN ATRIBUTOS
+ */
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
+export interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
   constructor(
-    private http : HttpClientModule,
-  ) { }
+    private http : HttpClient,
+     ){}
+  
+    getUsers(){
+      /**
+       * EL NUEVO MODULO DE HTTPCLIENT DEVUELVE LOS OBJETOS EN JSON
+       * BY DEFAULT, ASI QUE NO HACE FALTA MAPEAR NADA
+       */
+      return this.http.get(`${baseUrl}/users`)
+    }
+    getPosts(){
+      return this.http.get(`${baseUrl}/posts`)
+    }
+    getTodos(){
+      return this.http.get(`${baseUrl}/todos`)
+    }
+
 }
